93efb48f7c169006bd4ec4d108992f8289e3d4ac
邹伟 <zou.wei@z2a.com.cn>
2019-06-05 19:20:44 +0800

    修改ResponseHandler

--- src/main/java/com/github/zw201913/simplehttp/core/handler/ResponseHandler.java
+++ src/main/java/com/github/zw201913/simplehttp/core/handler/ResponseHandler.java
@@ -2,7 +2,14 @@ package com.github.zw201913.simplehttp.core.handler;
 
 import okhttp3.Response;
 
-public interface ResponseHandler<T> {
-
+/** @author zouwei */
+public interface ResponseHandler {
+    /**
+     * 处理响应对象
+     *
+     * @param response
+     * @param <T>
+     * @return
+     */
     <T> T handle(Response response);
 }
